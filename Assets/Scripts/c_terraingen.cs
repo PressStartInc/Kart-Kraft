3d776f9b510765ed2c82ee9babf817b830b045d8
daxiongmao87 <daxiongmao87@gmail.com>
2016-02-14 13:42:12 -0600

    Early implementation of terrain generator. p_terrainCell = placeholder prefab for block
    i_resolution = grid resolution. Recommend having it at 128 for now
    
    f_sampleDetails[0-2] = Sample size of perlin noise area.  This is not
    dictate resolution, but the area of the perlin noise we are sampling.
    0 = main shape, 1 = medium detail, 2 = fine detail
    
    f_altDampenStrength = altitude dampen strength modifier. 0 = none, 1 = full
    f_altDampenStart = at what altitude (0.0 - 1.0) will the dampening start to
    taper (Should rename this variable to prevent confusion)
    
    s_blendTypes[0-1] = type of blends: add, subtract, average.  May add more if
    proves to be useful.  0 = medium detail, 1 = fine detail
    
    s_blendStrengths = strength of blend.  0 = none, 1 = full.
    Items 0 = medium detail, 1 = fine detail
    
    NOTE: Fine detail is not implemented yet.

---
+++ Assets/Scripts/c_terraingen.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class c_terraingen : MonoBehaviour {
+
+	public GameObject p_terrainCell;
+	public int i_resolution;
+	public float[] f_sampleDetails;
+	private int[,] i_heightmap;
+	private Vector2 v2_position; 
+	private Vector2[] v2_perlinOrigins;
+	public float f_altDampenStrength;
+	public float f_altDampenStart;
+	public string[] s_blendTypes; //add, subtract, average
+	public float[] f_blendStrengths;
+	
+	// Use this for initialization
+	void Start () {
+	GenerateTerrain();
+	Camera.main.transform.position = new Vector3(i_resolution/2,i_resolution/2,20);
+	Camera.main.transform.Translate(0,0,-i_resolution/2);
+	}
+	void Update() {
+		if(Input.GetKey(KeyCode.Space)) Application.LoadLevel(Application.loadedLevel);
+	}
+	// Simple implementation of terrain generation using built-in perlin noise 
+	void GenerateTerrain() {
+		//create heightmap with size parameters
+		i_heightmap = new int[i_resolution,i_resolution];
+		//Pick random positions on the perlin noise texture
+		v2_perlinOrigins = new Vector2[3];
+		v2_perlinOrigins[0] = new Vector2(Random.Range(0f,1f),Random.Range(0f,1f));
+		v2_perlinOrigins[1] = new Vector2(Random.Range(0f,1f),Random.Range(0f,1f));
+		v2_perlinOrigins[2] = new Vector2(Random.Range(0f,1f),Random.Range(0f,1f));
+		//Sample perlin noise texture for height starting from origins
+		for(int i = 0; i < i_resolution; i++){
+			for(int j = 0; j < i_resolution; j++){
+				float f_sampleSum;
+				//Main shape
+				float f_sampleStep = f_sampleDetails[0]/(float)i_resolution;
+				Vector2 v2_samplePosition = new Vector2(v2_perlinOrigins[0].x+(f_sampleStep*i),v2_perlinOrigins[0].y+(f_sampleStep*j));
+				if(v2_samplePosition.x > 10.0) v2_samplePosition.x+= -1;
+				if(v2_samplePosition.y > 10.0) v2_samplePosition.y+= -1;
+				float f_rawSample = Mathf.PerlinNoise(v2_samplePosition.x,v2_samplePosition.y);
+				//Apply Altitude Dampener
+				f_rawSample = AltitudeDampen(f_rawSample);
+				f_sampleSum = f_rawSample;
+				
+				//Medium detail
+				f_sampleStep = f_sampleDetails[1]/(float)i_resolution;
+				v2_samplePosition = new Vector2(v2_perlinOrigins[1].x+(f_sampleStep*i),v2_perlinOrigins[1].y+(f_sampleStep*j));
+				if(v2_samplePosition.x > 10.0) v2_samplePosition.x+= -1;
+				if(v2_samplePosition.y > 10.0) v2_samplePosition.y+= -1;
+				f_rawSample = Mathf.PerlinNoise(v2_samplePosition.x,v2_samplePosition.y);
+				f_rawSample = AltitudeDampen(f_rawSample);
+				switch(s_blendTypes[0]) {
+					case "add":
+						f_sampleSum=Mathf.Clamp(f_sampleSum+(f_rawSample*f_blendStrengths[0]),0,1);
+						break;
+					case "subtract":
+						f_sampleSum=Mathf.Clamp(f_sampleSum-(f_rawSample*f_blendStrengths[0]),0,1);
+						break;
+					case "average":
+						f_sampleSum=Mathf.Clamp((f_sampleSum+Mathf.Lerp(f_sampleSum,f_rawSample,f_blendStrengths[0]))/2f,0,1);
+						break;
+				}
+				//Create blocks
+				i_heightmap[i,j] = (int)(f_sampleSum*i_resolution);
+				Instantiate(p_terrainCell,new Vector3(i,i_heightmap[i,j],j),Quaternion.identity);
+			}
+		}
+	}
+	float AltitudeDampen(float f_rawSample){
+		float f_altDampenHeight = 1f-f_altDampenStart;
+				
+		if(f_rawSample < f_altDampenStart) f_rawSample = f_rawSample*(1-f_altDampenStrength);
+		//else f_rawSample = (f_altDampenStart*(1-f_altDampenStrength))+f_rawSample*(f_altDampenStrength*(Mathf.Clamp((f_rawSample-f_altDampenStart)/f_altDampenHeight,0,1)));
+		else f_rawSample = Mathf.Lerp(f_rawSample*(1-f_altDampenStrength),f_rawSample,(f_rawSample-f_altDampenStart)/f_altDampenHeight);
+		return f_rawSample;
+	}
+}
+
