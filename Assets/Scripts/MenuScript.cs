a56cf6940413d7e513605f702d3b07ae425c0554
Trever Berryman <tberry5@lsu.edu>
2016-02-26 12:58:29 -0600

    Interface Progress

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public Canvas QuitMenu;
+	public Button StartText, ExitText;
+
+	void Start () {
+		QuitMenu = QuitMenu.GetComponent<Canvas> ();
+		StartText = StartText.GetComponent<Button> ();
+		ExitText = ExitText.GetComponent<Button> ();
+		QuitMenu.enabled = false;
+	}
+	public void ExitPress () {
+		QuitMenu.enabled = true;
+		StartText.enabled = false;
+		ExitText.enabled = false;
+	}
+	public void NoPress () {
+		QuitMenu.enabled = false;
+		StartText.enabled = true;
+		ExitText.enabled = true;
+	}
+	public void StartLevel () {
+		SceneManager.LoadScene ("KART_DEV");
+	}
+	public void ExitGame () {
+		Application.Quit();
+	}
+}
