e1d5b9fa75d109f3553044cf277bc7486a427dfc
Trever Berryman <tberry5@lsu.edu>
2016-04-28 00:38:39 -0500

    PowerUpImage Script Progress

---
+++ Assets/Scripts/PowerUpImage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpImage : MonoBehaviour {
+	public InvController script;
+	public GameObject a;
+	public 
+	void Start () {
+		script = a.GetComponent<InvController>();
+	}
+	void FixedUpdate () {
+		/*
+		if (InvController.heldItem == 1) {
+			//a.transform.GetChild(0).GetComponent<Image>.overrideSprite =  Resources.Load<Sprite>("Textures/sprite");
+		} 
+		else if (InvController.heldItem == 2) {
+
+		}
+		else if (InvController.heldItem == 3) {
+
+		}
+		*/
+	}
+}
